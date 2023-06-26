d5370e154aeaea527df4a47bb958c2394fd0f5c6
CrossNox <ijmermet@gmail.com>
2023-06-25 23:46:35 -0300

    fix ABV and IBU sliders ranges

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ChakraProvider,
   Container,
@@ -16,68 +16,85 @@ import {
   SliderThumb,
   Tooltip,
   Checkbox,
-} from '@chakra-ui/react';
-import beers from './beers.json'
-import { Engine } from 'json-rules-engine'
+} from "@chakra-ui/react";
+import beers from "./beers.json";
+import { Engine } from "json-rules-engine";
 
-const engine = new Engine(beers.decisions)
+const engine = new Engine(beers.decisions);
 
-const colors = ['clara', 'rubia', 'roja', 'negra']
-const cuerpos = ['ligero', 'medio', 'completo', 'cremoso']
-const maltas = ['pálida', 'caramelo', 'tostada', 'chocolate', 'negra']
-const IBUs = ['sin amargor', 'poco amargor', 'amarga', 'amargor moderado', 'muy amarga']
-const ABVs = ['sin alcohol', 'bajo', 'moderado', 'elevado', 'muy elevado']
-const maridajes = ['salado', 'torta', 'carnes rojas', 'carnes blancas', 'quesos', 'sola']
+const colors = ["clara", "rubia", "roja", "negra"];
+const cuerpos = ["ligero", "medio", "completo", "cremoso"];
+const maltas = ["pálida", "caramelo", "tostada", "chocolate", "negra"];
+const IBUs = [
+  "sin amargor",
+  "poco amargor",
+  "amarga",
+  "amargor moderado",
+  "muy amarga",
+];
+const ABVs = ["sin alcohol", "bajo", "moderado", "elevado", "muy elevado"];
+const maridajes = [
+  "salado",
+  "torta",
+  "carnes rojas",
+  "carnes blancas",
+  "quesos",
+  "sola",
+];
 
 function App() {
-  const [beer, setBeer] = React.useState(null)
+  const [beer, setBeer] = React.useState(null);
 
-  const [color, setColor] = React.useState(null)
-  const [cuerpo, setCuerpo] = React.useState(null)
-  const [malta, setMalta] = React.useState(null)
-  const [IBU, setIBU] = React.useState(null)
-  const [ABV, setABV] = React.useState(null)
-  const [maridaje, setMaridaje] = React.useState([])
+  const [color, setColor] = React.useState(null);
+  const [cuerpo, setCuerpo] = React.useState(null);
+  const [malta, setMalta] = React.useState(null);
+  const [IBU, setIBU] = React.useState(null);
+  const [ABV, setABV] = React.useState(null);
+  const [maridaje, setMaridaje] = React.useState([]);
 
   const toggleMaridaje = (m) => {
     if (maridaje.includes(m)) {
-      setMaridaje(maridaje.filter(x => x !== m))
+      setMaridaje(maridaje.filter((x) => x !== m));
     } else {
-      setMaridaje([...maridaje, m])
+      setMaridaje([...maridaje, m]);
     }
-  }
+  };
 
   React.useEffect(() => {
-    const b = async () =>
-      setBeer(await engine
-        .run({
-          color,
-          cuerpo,
-          malta,
-          IBU,
-          ABV,
-          maridaje,
+    const b = async () => {
+      const facts = {
+        color: color,
+        cuerpo: cuerpo,
+        malta: malta,
+        IBU: IBU,
+        ABV: ABV,
+        maridaje: maridaje,
+      };
+      setBeer(
+        await engine.run(facts).then(({ results }) => {
+          return results[0]?.event.type;
         })
-        .then(({ results }) => {
-          return results[0]?.event.type
-        }))
-    b()
-  }, [ABV, IBU, color, cuerpo, malta, maridaje])
+      );
+    };
+    b();
+  }, [ABV, IBU, color, cuerpo, malta, maridaje]);
 
   return (
     <ChakraProvider>
-      <Box h="100vh" w="100vw" bgGradient="linear(135deg, cornsilk 0%, lemonchiffon 100%)">
+      <Box
+        h="100vh"
+        w="100vw"
+        bgGradient="linear(135deg, cornsilk 0%, lemonchiffon 100%)"
+      >
         <Container maxW="5xl">
-          <Stack
-            align={'center'}
-            spacing={10}
-            py={28}
-          >
+          <Stack align={"center"} spacing={10} py={28}>
             <Heading fontSize="6xl">
-              Beer{' '}
-              <Text as="span" color={'orange.400'}>XP</Text>
+              Beer{" "}
+              <Text as="span" color={"orange.400"}>
+                XP
+              </Text>
             </Heading>
-            <Stack spacing={6} direction={'row'}>
+            <Stack spacing={6} direction={"row"}>
               <Box
                 borderWidth="2px"
                 rounded="lg"
@@ -90,51 +107,55 @@ function App() {
               >
                 <Flex>
                   <FormControl mr={8}>
-                    <FormLabel>
-                      Color
-                    </FormLabel>
-                    <Select placeholder='Color'
-                      onChange={e => setColor(e.target.value)}
+                    <FormLabel>Color</FormLabel>
+                    <Select
+                      placeholder="Color"
+                      onChange={(e) => setColor(e.target.value)}
                     >
-                      {colors.map(color =>
-                        <option key={color} value={color}>{color}</option>)}
+                      {colors.map((color) => (
+                        <option key={color} value={color}>
+                          {color}
+                        </option>
+                      ))}
                     </Select>
                   </FormControl>
 
                   <FormControl>
-                    <FormLabel>
-                      Cuerpo
-                    </FormLabel>
-                    <Select placeholder='Cuerpo'
-                      onChange={e => setCuerpo(e.target.value)}
+                    <FormLabel>Cuerpo</FormLabel>
+                    <Select
+                      placeholder="Cuerpo"
+                      onChange={(e) => setCuerpo(e.target.value)}
                     >
-                      {cuerpos.map(cuerpo =>
-                        <option key={cuerpo} value={cuerpo}>{cuerpo}</option>)}
+                      {cuerpos.map((cuerpo) => (
+                        <option key={cuerpo} value={cuerpo}>
+                          {cuerpo}
+                        </option>
+                      ))}
                     </Select>
                   </FormControl>
                 </Flex>
 
                 <FormControl mt="2%">
-                  <FormLabel>
-                    Malta
-                  </FormLabel>
-                  <Select placeholder='Malta'
-                    onChange={e => setMalta(e.target.value)}
+                  <FormLabel>Malta</FormLabel>
+                  <Select
+                    placeholder="Malta"
+                    onChange={(e) => setMalta(e.target.value)}
                   >
-                    {maltas.map(malta =>
-                      <option key={malta} value={malta}>{malta}</option>)}
+                    {maltas.map((malta) => (
+                      <option key={malta} value={malta}>
+                        {malta}
+                      </option>
+                    ))}
                   </Select>
                 </FormControl>
 
                 <FormControl mt={8}>
                   <Flex>
                     <FormControl mr={8}>
-                      <FormLabel>
-                        ABV
-                      </FormLabel>
+                      <FormLabel>ABV</FormLabel>
                       <Slider
-                        min={0}
-                        max={ABVs.length - 1}
+                        min={1}
+                        max={ABVs.length}
                         step={1}
                         onChange={setABV}
                         colorScheme="orange"
@@ -144,8 +165,8 @@ function App() {
                         </SliderTrack>
                         <Tooltip
                           hasArrow
-                          bg='orange.400'
-                          placement='bottom'
+                          bg="orange.400"
+                          placement="bottom"
                           isOpen={ABV !== null}
                           label={ABVs[ABV]}
                         >
@@ -155,12 +176,10 @@ function App() {
                     </FormControl>
 
                     <FormControl>
-                      <FormLabel>
-                        IBU
-                      </FormLabel>
+                      <FormLabel>IBU</FormLabel>
                       <Slider
-                        min={0}
-                        max={IBUs.length - 1}
+                        min={1}
+                        max={IBUs.length}
                         step={1}
                         onChange={setIBU}
                         colorScheme="orange"
@@ -170,8 +189,8 @@ function App() {
                         </SliderTrack>
                         <Tooltip
                           hasArrow
-                          bg='orange.400'
-                          placement='bottom'
+                          bg="orange.400"
+                          placement="bottom"
                           isOpen={IBU !== null}
                           label={IBUs[IBU]}
                         >
@@ -182,27 +201,33 @@ function App() {
                   </Flex>
 
                   <FormControl mt="4%">
-                    <FormLabel>
-                      Maridajes
-                    </FormLabel>
-                    <Stack spacing={5} direction='row'>
-                      {maridajes.map(m =>
-                        <Checkbox key={m} colorScheme='orange'
+                    <FormLabel>Maridajes</FormLabel>
+                    <Stack spacing={5} direction="row">
+                      {maridajes.map((m) => (
+                        <Checkbox
+                          key={m}
+                          colorScheme="orange"
                           isChecked={maridaje.includes(m)}
-                          onChange={() => { toggleMaridaje(m) }}
+                          onChange={() => {
+                            toggleMaridaje(m);
+                          }}
                         >
                           {m}
-                        </Checkbox>)
-                      }
+                        </Checkbox>
+                      ))}
                     </Stack>
                   </FormControl>
                 </FormControl>
               </Box>
             </Stack>
-            {beer && (<Heading fontSize="4xl">
-              Tu cerveza perfecta es...{' '}
-              <Text as="span" color={'orange.500'}>{beer}</Text>
-            </Heading>)}
+            {beer && (
+              <Heading fontSize="4xl">
+                Tu cerveza perfecta es...{" "}
+                <Text as="span" color={"orange.500"}>
+                  {beer}
+                </Text>
+              </Heading>
+            )}
           </Stack>
         </Container>
       </Box>
